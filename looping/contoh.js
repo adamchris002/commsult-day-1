d21685881034027740d434935b2cac4ea35d708b
unknown <adam2510025@gmail.com>
2022-10-10 14:42:24 +0700

    commsult day 1

--- looping/contoh.js
+++ looping/contoh.js
@@ -1,10 +1,40 @@
-const arr = [1, 20, 45, 67, 90]
+// const arr = [1, 20, 45, 67, 90]
 
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] %2 == 0) {
-        console.log(arr[i] + " adalah genap")
-    }
-    else {
-        console.log(arr[i] + " adalah ganjil")
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] %2 === 0) {
+//         console.log(arr[i] + " adalah genap")
+//     }
+//     else if (arr[i] %2 !== 0) {
+//         console.log(arr[i] + " adalah ganjil")
+//     }
+//     else {
+//         console.log(arr[i] + " bukan sebuah bilangan")
+//     }
+// }
+
+//soal latihan
+
+var turtle = "turtle"
+var number = 1000
+
+var total = turtle;
+
+//menggunakan for
+const word1 = "turtle";
+const word2 = "153";
+
+let dots = "";
+for (let i = 0; i < 30 - word1.length - word2.length; i++) {
+    dots += ".";
+}
+console.log(word1 + dots + word2);
+
+// menggunakan while
+while (total.length !== 30) {
+    total += "."
+    if (total.length + number.toString().length === 30) {
+        total += number.toString()
     }
 }
+
+// console.log(total)
