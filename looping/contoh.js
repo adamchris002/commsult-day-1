48d21bf35e55b3399e4f4566ed8c4ebca6c41e92
unknown <adam2510025@gmail.com>
2022-10-10 13:45:49 +0700

    latihan day-1

---
+++ looping/contoh.js
@@ -0,0 +1,10 @@
+const arr = [1, 20, 45, 67, 90]
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] %2 == 0) {
+        console.log(arr[i] + " adalah genap")
+    }
+    else {
+        console.log(arr[i] + " adalah ganjil")
+    }
+}
