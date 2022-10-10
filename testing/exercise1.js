a4e2024d03180a8ab25cd80ba943ddf2176122e6
unknown <adam2510025@gmail.com>
2022-10-10 13:46:21 +0700

    latihan day-1

---
+++ testing/exercise1.js
@@ -0,0 +1,15 @@
+var time = 6
+var age = 13
+
+if (age < 13 && time < 5) {
+    console.log("Harganya adalah $2")
+}
+else if (age < 13 && time >= 5) {
+    console.log("Harganya adalah $4")
+}
+else if (age >= 13 && time >= 5) {
+    console.log("Harganya adalah $8")
+}
+else {
+    console.log("Harganya adalah $5")
+}
---
+++ testing/helloworld.js
@@ -0,0 +1,34 @@
+// console.log("Hello World!")
+
+var a = 1
+const b = 2
+let c = 3
+
+
+// var number = 1
+var str = "string"
+
+//array
+var arr = [1, 2, 3]
+// console.log(arr[2])
+//object
+var names = {
+    Adam: {
+        age: 20,
+        last_name: "Christoper",
+        hobby: "Main game"
+    }
+}
+// console.log(names.Adam.last_name)
+
+let number = 12
+
+if (number %2 == 0) {
+    console.log("Nomor tersebut genap")
+}
+else {
+    console.log("Nomor tersebut ganjil")
+}
+
+
+
